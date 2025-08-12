4cdaa15da360532274284c05359bc8d94ccb0e02
Woojin <119497359+WWOJJIN@users.noreply.github.com>
2025-08-12 15:51:12 +0900

    추추

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Nav from './components/Nav'
-import About from './pages/About'
-import Board from './pages/Board'
-import Home from './pages/Home'
-import Notfound from './pages/Notfound'
-import BoardDetail from './pages/BoardDetail'
+import TodoList from './components/TodoList'
+
 function App() {
 
   return (
     <div>
-      <Nav />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/board' element={<Board />} />
-        <Route path='/board/:id' element={<BoardDetail />} />
-        <Route path='*' element={<Notfound />} />
-      </Routes>
-
+      <TodoList />
     </div>
   )
 }
