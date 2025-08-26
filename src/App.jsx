10addb1ffad273eb27d915c8bb67a4376353d16b
Woojin <119497359+WWOJJIN@users.noreply.github.com>
2025-08-26 17:02:31 +0900

    추추

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Nav from './components/Nav'
 import About from './pages/About'
 import Board from './pages/Board'
@@ -8,19 +8,19 @@ import Home from './pages/Home'
 import Notfound from './pages/Notfound'
 import BoardDetail from './pages/BoardDetail'
 import Footer from './components/Footer'
-function App() {
+function App() {//
 
   return (
     <div>
-      <Nav/>
+      <Nav />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/board' element={<Board/>}/>
-        <Route path='/board/:id' element={<BoardDetail/>}/>
-        <Route path='*' element={<Notfound/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/board' element={<Board />} />
+        <Route path='/board/:id' element={<BoardDetail />} />
+        <Route path='*' element={<Notfound />} />
       </Routes>
-    <Footer/>
+      <Footer />
     </div>
   )
 }
